e9f52e98b2c20b52fe91ec5312987532710a5c16
Donovan <donovanjuniogomez@gmail.com>
2022-02-08 00:40:35 -0800

    add hella properties from index to Form component

--- components/Form.js
+++ components/Form.js
@@ -3,20 +3,39 @@ import PlayerOneCharacter from "./PlayerOneCharacter";
 import PlayerTwoCharacter from "./PlayerTwoCharacter";
 import PlayerOneInput from "./PlayerOneInput";
 import PlayerTwoInput from "./PlayerTwoInput";
-import { Flex } from "@chakra-ui/react";
+import { Flex, Button } from "@chakra-ui/react";
 
-export default function Form() {
+export default function Form({
+  characterOne,
+  characterTwo,
+  setCharacterOne,
+  setCharacterTwo,
+  playerOne,
+  setPlayerOne,
+  playerTwo,
+  setPlayerTwo,
+  characterSearchHandler,
+}) {
   return (
     <form>
       <h2>This is a form thing yo</h2>
       <Flex>
-        <PlayerOneCharacter />
-        <PlayerTwoCharacter />
+        <PlayerOneCharacter
+          characterOne={characterOne}
+          setCharacterOne={setCharacterOne}
+        />
+        <PlayerTwoCharacter
+          characterTwo={characterTwo}
+          setCharacterTwo={setCharacterTwo}
+        />
       </Flex>
       <Flex>
-        <PlayerOneInput />
-        <PlayerTwoInput />
+        <PlayerOneInput playerOne={playerOne} setPlayerOne={setPlayerOne} />
+        <PlayerTwoInput playerTwo={playerTwo} setPlayerTwo={setPlayerTwo} />
       </Flex>
+      <Button type="submit" onClick={characterSearchHandler}>
+        Searcg
+      </Button>
     </form>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,34 @@
 import styles from "../styles/Home.module.css";
 import Form from "../components/Form";
 import { Flex } from "@chakra-ui/react";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [characterOne, setCharacterOne] = useState("");
+  const [characterTwo, setCharacterTwo] = useState("");
+  const [playerOne, setPlayerOne] = useState("");
+  const [playerTwo, setPlayerTwo] = useState("");
+
+  // search character function
+  const characterSearchHandler = (e) => {
+    console.log("handler worked!");
+  };
+
   return (
     <Flex as="main" minH="100vh" direction="column">
       <Flex flex="1" border="1px" borderColor="gray.600">
         <h2>Hello</h2>
-        <Form />
+        <Form
+          characterOne={characterOne}
+          characterTwo={characterTwo}
+          setCharacterOne={setCharacterOne}
+          setCharacterTwo={setCharacterTwo}
+          playerOne={playerOne}
+          playerTwo={playerTwo}
+          setPlayerOne={setPlayerOne}
+          setPlayerTwo={setPlayerTwo}
+          characterSearchHandler={characterSearchHandler}
+        />
       </Flex>
       <Flex flex="1">
         <h2>Hello Bish</h2>
